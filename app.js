c09c78f40b76374da99c088ecb787b3cf11330d3
Tobias <tobiasvega1210@gmail.com>
2024-07-02 17:53:55 -0300

    Cambios

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
-const express = require('express')
-const books = require('./book')
+const express = require('express');
+const books = require('./db')
 
 const app = express();
 
 app.use(express.json())
-app.use(express.text());
+app.use(express.text())
 
 app.get('/books', (req,res) => {
     if(books.length === 0) {
-        res.status(204)
+        return res.status(204).send()
     }
     res.json(books)
 })
 
-app.get("/books/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const getBook = books.find((title) => title.id === id);
-    if (!getBook) {
-    res.status(204);
-    }
-    res.json(getBook);
-});
+app.get('/books/:id', (req,res) => {
+    const id = parseInt(req.params.id)
 
-app.post('/books', (req,res)=> {
-    const id = new Date().getTime()
+    const getBook = books.find((book)=> book.id === id)
+
+    if (!getBook) {
+        return res.status(404).send('el libro no existe')
+    }
+
+    res.json(getBook)
+})
+
+app.post('/books', (req,res) => {
+    const generarId = new Date().getTime()
+
     let { title, author, year } = req.body
 
     title = title.trim();
-    author = author.trim();
+    author = author.trim()
+    year = parseInt(year)
 
     if(typeof year === "string") {
-        return res.status(400).send("El año debe ser un número")
+        return res.status(400).send('El año debe ser un número')
     }
 
     if (!title || !author || !year) {
-        return res.status(400).send("Datos faltantes")
+        return res.status(400).send('Datos faltantes')
     }
 
     const repeatBook = books.find((book) => book.title === title)
     if(repeatBook) {
-        return res.status(401).send("Ya existe el libro")
+        return res.status(401).send('Ya existe el libro')
     }
 
     books.push({
-        id: id,
+        id: generarId,
         title: title,
         author: author,
         year: +year
@@ -52,44 +57,39 @@ app.post('/books', (req,res)=> {
     res.send('Libro creado correctamente')
 })
 
-app.put('/books/:id', (req,res)=>{
+app.put('/books/:id', (req,res) => {
     const id = parseInt(req.params.id)
 
-    const { title, author, year } = req.body;
+    const { title, author, year } = req.body
 
     if(typeof year === "string") {
-        return res.status(400).send("El año debe ser un número")
+        return res.status(400).send('El año debe ser un número')
     }
 
-    const bookIndex = books.findIndex((title) => title.id === id)
+    const indexBook = books.findIndex((book) => book.id === id)
 
-    if(bookIndex === -1) {
-        return res.status(400).send("No existe el libro")
+    if(indexBook === -1) {
+        return res.status(400).send('No existe el libro')
     }
 
-    books[bookIndex].title = title.trim()
-    books[bookIndex].author = author.trim()
-    books[bookIndex].year = +year
+    books[indexBook].title = title.trim()
+    books[indexBook].author = author.trim()
+    books[indexBook].year = parseInt(year)
 
     res.send('Se actualizaron los datos')
 })
 
-
 app.delete('/books/:id', (req,res) => {
     const id = parseInt(req.params.id)
 
-    const getBook = books.findIndex((title) => title.id === id)
+    const getBook = books.findIndex((book) => book.id === id)
 
     if(getBook === -1) {
         return res.status(404).send('No existe el libro')
     } else {
-        books.splice(getBook, 1);
-        res.send('Libro eliminado')
+        books.splice(getBook, 1)
+        res.send('libro eliminado')
     }
-
-    
 })
 
-
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
-
+app.listen(3000, ()=> console.log("Servidor corriendo"))
